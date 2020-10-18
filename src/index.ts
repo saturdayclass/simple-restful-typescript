62f89f1b025bb55d023ed5c56da0105ff94c3af6
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2020-10-18 17:19:39 +0700

    add: library supported in development

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,34 @@
 import express, { Application, Request, Response } from 'express';
+import morgan from 'morgan';
+import compression from 'compression';
+import helmet from 'helmet';
+import cors from 'cors';
 
 class App {
   public app: Application;
 
   constructor() {
     this.app = express();
+    this.plugins();
     this.routes();
+  }
+
+  protected plugins(): void {
+    this.app.use(express.json());
+    this.app.use(morgan('dev'));
+    this.app.use(compression());
+    this.app.use(helmet());
+    this.app.use(cors());
   }
 
   protected routes(): void {
     this.app.route('/').get((req: Request, res: Response): void => {
       res.send('Hi');
+    });
+
+    this.app.route('/').post((req: Request, res: Response): void => {
+      res.send(req.body);
+      console.log(req.body);
     });
   }
 }
